f37babc574b7c54c3b9463dad4258159b30c96ed
Cesar <cesarbaezmoya06@gmail.com>
2025-11-20 20:11:55 -0600

    agregar escritura y método bubbleSort

--- bubbleFiles.java
+++ bubbleFiles.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class bubbleFiles {
 
@@ -47,5 +49,38 @@ public class bubbleFiles {
             System.out.println("Error al leer el archivo: " + e.toString());
         }
         return array;
+    }
+
+    public void writeIntArrayToFile(String fileName, int[] array){
+        FileWriter file;
+        PrintWriter writer;
+        try{
+            file = new FileWriter("c:\\archivos\\" + fileName);
+            writer = new PrintWriter(file);
+            for (int unDato : array)
+                writer.println(unDato);
+            file.close();
+        } catch (Exception e) {
+            System.out.println("Error al crear el archivo: " + e.toString());
+        }
+    }
+
+    // Bubble Sort para enteros (in-place)
+    public void bubbleSort(int[] arr){
+        if (arr == null) return;
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++){
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++){
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
     }
 }
